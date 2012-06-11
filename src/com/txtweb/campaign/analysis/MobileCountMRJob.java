e92b5f659fec6c163a102d8accdd74090cf74be8
Vijay Rayapati <amnigos@gmail.com>
2012-06-11 15:50:55 +0530

    Renamed package name to Kuliza utils

--- src/com/txtweb/campaign/analysis/MobileCountMRJob.java
+++ src/com/txtweb/campaign/analysis/MobileCountMRJob.java
@@ -12,7 +12,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-import com.placeiq.inputformat.KeyTextInputFormat;
+import com.kuliza.inputformat.KeyTextInputFormat;
         
 public class MobileCount {
         
