ae937f3d99724fdd76ce1636cbc8b8b27608e1be
Vijay Rayapati <amnigos@gmail.com>
2012-06-13 12:07:41 +0530

    Adding Hadoop CustomInputFormat Implementation

---
+++ src/com/txtweb/campaign/analysis/KeyTextInputFormat.java
@@ -0,0 +1,32 @@
+package com.txtweb.campaign.analysis;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+public class KeyTextInputFormat extends FileInputFormat<Text, Text> {
+
+	
+	  @Override
+	  public RecordReader<Text, Text> 
+	    createRecordReader(InputSplit split,
+	                       TaskAttemptContext context) {
+	    return new KeyTextRecordReader();
+	  }
+
+	  @Override
+	  protected boolean isSplitable(JobContext context, Path file) {
+	    CompressionCodec codec = 
+	      new CompressionCodecFactory(context.getConfiguration()).getCodec(file);
+	    return codec == null;
+	  }
+
+
+
+}
